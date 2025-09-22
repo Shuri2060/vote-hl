7cd11ffd227b03073aaf5765fd48375e772b733a
Shuri2060 <29679284+Shuri2060@users.noreply.github.com>
2025-09-22 13:39:32 +0900

    ui for polls

--- docs/index.js
+++ docs/index.js
@@ -15,4 +15,51 @@ const hyGovContract = new ethers.Contract(
     provider,
 )
 
+async function loadPolls() {
+    const polls = []
+    const pollCount = await hyGovContract.pollCount()
+    for (let i = 0; i < pollCount; i++) {
+        const [question, choices, startDate, endDate, minStake, creator] = await hyGovContract.polls(pollCount)
+        polls[i] = { question, choices, startDate, endDate, minStake, creator }
+
+        // struct Poll {
+        //     string question;
+        //     string[] choices;
+        //     uint256 startDate;
+        //     uint256 endDate;
+        //     uint256 minStake;
+        //     address creator
+        // }
+    }
+    return polls
+}
+
+function displayPolls(polls) {
+
+}
+
+////////////////////////////////////
+// Test
+
 console.log(await hyGovContract.oracle())
+
+console.log('')
+
+console.log(await hyGovContract.polls(0))
+console.log((await hyGovContract.polls(0))[0]) // question
+console.log((await hyGovContract.polls(0))[1]) // description
+console.log((await hyGovContract.polls(0))[2]) // startdate
+console.log((await hyGovContract.polls(0))[3]) // enddate
+console.log((await hyGovContract.polls(0))[4]) // minstake
+console.log((await hyGovContract.polls(0))[5]) // creator
+
+console.log('')
+
+console.log(await hyGovContract.votes(0))
+
+
+
+//     struct Vote {
+//     address[] voters
+//     uint256[] choices;
+//     uint256 timestamp
